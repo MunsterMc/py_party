7507e980f0c17a986442b9d3b1eb648c4fe899c7
MunsterMc <30962666+MunsterMc@users.noreply.github.com>
2019-05-02 11:15:28 +0200

    Add files via upload

--- projet.py
+++ projet.py
@@ -15,18 +15,28 @@ import sys
 PLATEAU = [3,3,2,1,2,3,2,1,2,1,3,1,2,1,2,1,2,1,2,2,3,1,2,3,1,2,1,2,2,1,3,2]
 ### Arrière-plan
 bg = 'background.gif'
-screen.addshape(bg)
-turtle.Turtle().shape(bg)
-cree_plateau(PLATEAU)
+screen.addshape(bg) #Ajoute l'arrière-plan
 
 def nv_etoile():
     pos_etoile = randint(0,len(PLATEAU)-1)
     place_etoile(pos_etoile,PLATEAU)
+    
+#Fct des objets
+def vol5():    #Vole 5 pièces à l'adversaire choisi
+    choixAdv = input("A quel adversaire voulez vous voler des pièces?:")
+    
+    
 #Fct Objet aléatoire
 def objet_gain():
     return ["Vol 5 Pièces","Vol étoile","+3","-3"][randint(0,3)]
 
 def initialisation():
+    efface_plateau()
+    
+    ###Place le plateau
+    turtle.Turtle().shape(bg)
+    cree_plateau(PLATEAU)
+    
     ### Les joueurs
     J1={"position":0,"pieces":5,"etoiles":0,"objets":[]}
     J2={"position":0,"pieces":5,"etoiles":0,"objets":[]}
@@ -37,14 +47,16 @@ def initialisation():
 
     ### Initialisation
     nv_etoile()
+    
     ##Vérification du nombre de joueurs
     nbr_joueurs=int(input("Nombre de joueurs :(1 à 4 Joueurs)\n"))
-    while nbr_joueurs not in (1,2,3,4):
+    while nbr_joueurs not in range(1,5):
         print("Le nombre de joueurs n'est pas entre 1 et 4")
         nbr_joueurs=int(input("Nombre de joueurs :(1 à 4 Joueurs)\n"))
         
     for i in range(nbr_joueurs): #Ajoute les dictionnaires pour le nombre de joueurs donnés
         JOUEURS.append(MAX_JOUEURS[i])    
+
 
     #Choix des pseudos
     for i in JOUEURS:
@@ -52,9 +64,10 @@ def initialisation():
     #Choix du nombre de tours
     nbr_tours = int(input("Choisissez le nombre de tours\n"))
 
+
     #Création des persos
     cree_perso(JOUEURS,PLATEAU)
-    choixObj = 0
+    
     ### Début du jeu
     #Lancement de dé
     for tour in range(nbr_tours+1):
@@ -67,12 +80,14 @@ def initialisation():
                     print(comptList,end=' •')
                     comptList+=1
                     print(j)
-                while choixObj>=-1:
+                choixObj=int(input("Donnez le numéro de l'objet que vous voulez utiliser: -1 pour annuler."))
+                while choixObj<=-1 and choixObj > len(i["objets"])-1:
                     choixObj=int(input("Donnez le numéro de l'objet que vous voulez utiliser: -1 pour annuler."))
                 if choixObj == -1:
                     print('Aucun objet utilisé')
                 elif i["objets"][choixObj] == "Vol 5 Pièces":
                     del i["objets"][choixObj]
+                    i["pieces"]+=5
                     vol5()
                 elif i["objets"][choixObj] == "Vol étoile":
                     del i["objets"][choixObj]
@@ -90,21 +105,37 @@ def initialisation():
                 if i["position"] == 32: ##Lorsque la position atteint 32 on remet les joueurs sur la case de départ (Pos 0)
                     i["position"] = 0
                 bouge_perso(JOUEURS,PLATEAU) #Fonction de interface.py pour bouger les joueurs sur le plateau
+            
             #Perso Bouge, conditions sur case d'arrivée
             if PLATEAU[i["position"]] == 1: ##Case Verte (Gagne 3 pièces)
                 print(i["pseudo"],"Vous aviez:",i["pieces"],"pièces")
                 i["pieces"] += 3
                 print("Vous avez maintenant:",i["pieces"],"pièces")
+                
             elif PLATEAU[i["position"]] == 2 and i["pieces"]>=3: ##Case Rouge, + de 3 pieces (Perd 3 pièces)
                 print(i["pseudo"],"Vous aviez:",i["pieces"],"pièces")
                 i["pieces"] -= 3
                 print("Vous avez maintenant:",i["pieces"],"pièces")
+                
             elif PLATEAU[i["position"]] == 2 and i["pieces"]<3: ##Case Rouge, - de 3 pieces (Perd 3 pièces)
                 print(i["pseudo"],"Vous aviez:",i["pieces"],"pièces")
                 i["pieces"] = 0
                 print("Vous avez maintenant:",i["pieces"],"pièces")
+                
             elif PLATEAU[i["position"]] == 3: ##Case Bleue (Objet aléatoire)
                 nouvObj = objet_gain()
                 i["objets"].append(nouvObj)
                 print("Vous avez gagné un objet")
                 print(i["objets"])
+                
+            if i["position"] == pos_etoile: ##Case étoile
+                confAchat = input("Voulez vous acheter cette étoile (Cout: 5 Pièces): N=Non Y=Oui").upper()
+                while confAchat == "Y" or confAchat == "N":
+                    confAchat = input("Voulez vous acheter cette étoile (Cout: 5 Pièces): N=Non Y=Oui")
+                if confAchat == "Y" and i["pieces"] >= 5:
+                    i["pieces"] -= 5
+                    turtle_star.reset()
+                    i["etoiles"] += 1
+                    nv_etoile()
+                elif confAchat == "Y" and i["pieces"] < 5:
+                    print("Vous n'avez pas assez de pièces")
